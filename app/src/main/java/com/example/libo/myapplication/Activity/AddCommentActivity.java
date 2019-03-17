0254fcc67ca173f92960c706efb27c96d12d776d
puyuan <puyuan@ualberta.ca>
2019-03-17 13:27:38 -0600

    top bar hid

--- app/src/main/java/com/example/libo/myapplication/Activity/AddCommentActivity.java
+++ app/src/main/java/com/example/libo/myapplication/Activity/AddCommentActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
@@ -17,6 +19,13 @@ public class AddCommentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+
         setContentView(R.layout.activity_add_comment);
         final RatingBar ratingRatingBar = (RatingBar) findViewById(R.id.CreateCommentRatingBar);
         Button submitButton = (Button) findViewById(R.id.CommentConfirmButton);
