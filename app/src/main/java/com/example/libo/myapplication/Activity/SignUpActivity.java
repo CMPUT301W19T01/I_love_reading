cbe5e20baf7c4b272dfdc00d48b0597904349677
BrightK7 <32251602+BrightK7@users.noreply.github.com>
2019-03-18 17:59:13 -0600

    Update SignUpActivity.java

--- app/src/main/java/com/example/libo/myapplication/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/libo/myapplication/Activity/SignUpActivity.java
@@ -1,33 +1,44 @@
 package com.example.libo.myapplication.Activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.libo.myapplication.R;
-import com.example.libo.myapplication.User;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     ProgressBar progressBar;
 
+    private static int REQUESCODE = 1;
+
     EditText editTextUsername,editTextPassword,editTextEmail;
     DatabaseReference databaseUser;
     private FirebaseAuth mAuth;
-
+    private ImageView photo;
+    private Uri uriProfileImage;
 
 
     @Override
@@ -46,6 +57,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
 
+
         databaseUser = FirebaseDatabase.getInstance().getReference("users");
         mAuth = FirebaseAuth.getInstance();
 
@@ -53,14 +65,25 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         findViewById(R.id.buttonCancel).setOnClickListener(this);
 
-    }
-    private void AddUser(String email, String username){
-        String id = mAuth.getUid();
-        User user = new User(email,username,id);
-        databaseUser.child(id).setValue(user);
-        Toast.makeText(this,"User add successful",Toast.LENGTH_LONG).show();
+
+        photo = (ImageView) findViewById(R.id.userphoto);
+
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opeangalley();
+            }
+        });
+
     }
 
+    private void opeangalley() {
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
 
 
     private void RegisterUser(){
@@ -115,12 +138,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()){
-                    AddUser(email,username);
+                    undateUserinfo(username, uriProfileImage,mAuth.getCurrentUser());
                     progressBar.setVisibility(View.GONE);
-
-                    Toast.makeText(getApplicationContext(),"User Registered Successfull",Toast.LENGTH_SHORT).show();
-
-                    Intent intent = new Intent(SignUpActivity.this, BasicActivity.class);
+                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
 
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
@@ -148,13 +168,61 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
+    private void undateUserinfo(final String username, final Uri uriProfileImage, final FirebaseUser currentUser) {
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("user_photo");
+        final StorageReference imageFilepath = mStorage.child(uriProfileImage.getLastPathSegment());
+        imageFilepath.putFile(uriProfileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageFilepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
 
+
+                        UserProfileChangeRequest profileupdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(username)
+                                .setPhotoUri(uriProfileImage)
+                                .build();
+
+                        currentUser.updateProfile(profileupdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(),"User Registered Successfull",Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null ) {
+
+            // the user has successfully picked an image
+            // we need to save its reference to a Uri variable
+            uriProfileImage = data.getData() ;
+            photo.setImageURI(uriProfileImage);
+
+
+        }
+    }
 
     @Override
 
     public void onClick (View view){
 
         switch (view.getId()){
+
+            case R.id.userphoto:
+
 
             case R.id.buttonSignUp:
 
@@ -166,7 +234,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
             case R.id.buttonCancel:
 
-                startActivity(new Intent(this, MainActivity.class));
+                startActivity(new Intent(this, LoginActivity.class));
 
         }
 
