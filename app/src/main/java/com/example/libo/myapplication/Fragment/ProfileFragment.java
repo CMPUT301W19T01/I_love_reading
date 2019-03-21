e4781537d4b7a9e9fb0725ef96d6c0ecc34fc851
YangAusDu <du3@ualberta.ca>
2019-03-21 14:36:54 -0600

    profile page

--- app/src/main/java/com/example/libo/myapplication/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/libo/myapplication/Fragment/ProfileFragment.java
@@ -13,19 +13,31 @@ import com.example.libo.myapplication.R;
 
 public class ProfileFragment extends Fragment {
     private Button button;
-    private TextView userNameTextView;
+    private TextView userNameView;
+    private TextView userEmailView;
+    private TextView userLocationView;
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.profile_page,container,false);
+
+
         return view;
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        userNameView = getActivity().findViewById(R.id.profileUserName);
+        userEmailView = getActivity().findViewById(R.id.profileUserEmail);
+        userLocationView = getActivity().findViewById(R.id.profileUserLocation);
+        userNameView.setText("user_1");
+        userEmailView.setText("b@gmail.com");
+        userLocationView.setText("southgate");
 
 
     }
