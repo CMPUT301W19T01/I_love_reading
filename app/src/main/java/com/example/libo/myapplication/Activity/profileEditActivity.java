26501dd5527c55f05d28ca041d3580b9bbc0a087
Puyuan Liu <puyuan@ualberta.ca>
2019-03-31 18:51:49 -0600

    Update profileEditActivity.java

--- app/src/main/java/com/example/libo/myapplication/Activity/profileEditActivity.java
+++ app/src/main/java/com/example/libo/myapplication/Activity/profileEditActivity.java
@@ -17,16 +17,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.libo.myapplication.Fragment.ProfileFragment;
+import com.example.libo.myapplication.Model.Users;
 import com.example.libo.myapplication.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -43,7 +49,11 @@ public class profileEditActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private Uri UserEditImage;
     final int GET_FROM_GALLERY = 2;
-    private Uri selectedImage;
+    private Uri selectedImage = null;
+    DatabaseReference databaseUser;
+    private FirebaseAuth mAuth;
+
+
 
 
     @Override
@@ -52,12 +62,16 @@ public class profileEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_edit);
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         userNameEditView = (EditText) findViewById(R.id.profileEditUserName);
-        userEmailEditView = findViewById(R.id.profileEditUserEmail);
+        userEmailEditView = findViewById(R.id.textView3);
         userIdEditView = findViewById(R.id.profileEditUserID);
         userContactEditView = (EditText) findViewById(R.id.profileEditUserContact);
         userEditImage = (ImageView) findViewById(R.id.profileEditUserImage);
 
         btn_save = findViewById(R.id.btn_saveProfile);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        databaseUser = FirebaseDatabase.getInstance().getReference("users");
 
 
         userEmailEditView.setText("CurrentEmail: " + user.getEmail());
@@ -104,6 +118,13 @@ public class profileEditActivity extends AppCompatActivity {
                                 }
                             }
                         });
+                if (selectedImage != null){
+                    FirebaseUser current_user = mAuth.getCurrentUser();
+                    String username = mAuth.getCurrentUser().getDisplayName();
+                    undateUserinfo(username, selectedImage,current_user);
+                }
+                finish();
+
             }
         });
     }
@@ -112,7 +133,7 @@ public class profileEditActivity extends AppCompatActivity {
 
         Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
         galleryIntent.setType("image/*");
-        startActivityForResult(galleryIntent,2);
+        startActivityForResult(galleryIntent,GET_FROM_GALLERY);
     }
 
     @Override
@@ -120,7 +141,7 @@ public class profileEditActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
             selectedImage = data.getData();
-            Bitmap bitmap = null;
+            Bitmap bitmap;
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
                 userEditImage.setImageBitmap(bitmap);
@@ -134,4 +155,44 @@ public class profileEditActivity extends AppCompatActivity {
         }
 
     }
+
+    private void undateUserinfo(final String username, final Uri uriProfileImage, final FirebaseUser currentUser) {
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference("user_photo/"+currentUser.getUid()+"jpg");
+        final StorageReference imageFilepath = mStorage.child(currentUser.getUid());
+        imageFilepath.putFile(uriProfileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageFilepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+
+                        UserProfileChangeRequest profileupdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(username)
+                                .setPhotoUri(uri)
+                                .build();
+                        Adduser(currentUser.getUid(),username,uri.toString(),currentUser.getEmail());
+                        currentUser.updateProfile(profileupdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(getApplicationContext(),"User Registered Successfull",Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void Adduser(String uid, String username, String photo_uri, String email) {
+        Users user = new Users(email,username,uid);
+        user.setPhoto(photo_uri);
+        databaseUser.child(uid).setValue(user);
+    }
 }
+
