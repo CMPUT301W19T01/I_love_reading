6ffa05dfec56a9e996785e0f52cc4886a55d2361
xingran35 <xingran@ualberta.ca>
2019-04-04 13:34:39 -0600

    Create otherProfilePopupActivity.java

---
+++ app/src/main/java/com/example/libo/myapplication/Activity/otherProfilePopupActivity.java
@@ -0,0 +1,16 @@
+package com.example.libo.myapplication.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class otherProfilePopupActivity extends AppCompatActivity {
+    public TextView otherNameView;
+    private TextView otherEmailView;
+    private TextView otherId;
+    private ImageView otherImage;
+
+
+    
+
+}
