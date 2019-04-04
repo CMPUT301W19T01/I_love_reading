3fd669e6fd033ae17cecd406bc4937f3c7e6e716
PuyuanLiu <puyuan@ualberta.ca>
2019-04-03 20:55:20 -0600

    activity

---
+++ app/src/main/java/com/example/libo/myapplication/Activity/UserSearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.libo.myapplication.Activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.libo.myapplication.R;
+
+public class UserSearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_search);
+    }
+
+}
